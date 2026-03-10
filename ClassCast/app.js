997035ec3d9f629d5a185db92398237687e5760d
wwilson-ui <wwilson@mtps.us>
2026-03-10 14:53:08 -0400

    Update app.js

--- ClassCast/app.js
+++ ClassCast/app.js
@@ -1,5 +1,5 @@
 // ==========================================
-// ClassCast - Unified Logic with Super Admin, Filters & Full Screen
+// ClassCast - Unified Logic with Super Admin, Filters & Full Screen Faster Fix
 // ==========================================
 
 let sb = null;
